d9fbb9e57f83d1c7585e020fce8c3973d98d5ee1
raniellschl <raniellschl@gmail.com>
2025-07-15 16:05:28 +0800

    Fixed production URLs

--- frontend/src/store/authUserStore.js
+++ frontend/src/store/authUserStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api";
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const authUserStore = create((set, get) => ({
   authUser: null,
