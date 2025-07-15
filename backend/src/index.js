01153c688af55b4faa2f48452db9371ef3201b5c
raniellschl <raniellschl@gmail.com>
2025-07-15 18:13:04 +0800

    path-to-regexp pt.3

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,18 +22,20 @@ app.use(
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
-app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
-app.all("/*splat", (req, res) => {
+
+app.use("*all", (req, res) => {
   res.status(404).json({
     message: `The URL ${req.originalUrl} doesn't exist`,
   });
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("*all", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
